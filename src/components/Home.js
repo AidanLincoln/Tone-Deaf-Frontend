2aaa18914566e388d087035f1fb394702e128b03
AidanLincoln <aidantlincoln@gmail.com>
2020-08-25 13:57:44 -0700

    Glowing sphere added to home page - why not?

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ export default class Home extends React.Component {
     render(){
         return(
             <div>
+                <div className="glow"></div>
                 <img className="homeLogo" src={Logo}></img>
                 <h4 className="homeDescription">Explore scales, create chords, and build chord progressions.</h4>
             </div>
